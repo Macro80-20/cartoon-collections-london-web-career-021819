1d435bbec291a3c8c4ea89aac13916a993bfb00d
fernando <fernando_lopezc@live.com>
2019-02-12 13:08:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,12 @@
 li = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(list)
-updated_array = list.map { |x| puts "#{list.index(x)+1}. #{x}"}
+updated_array = list.each_with_index do
+  |name,index| puts "#{index+1}. #{name}"
+end
   end
-
-def summon_captain_planet# code an argument here
-  # Your code here
+#updated_array = list.map { |x| puts "#{list.index(x)+1}. #{x}"}
+def summon_captain_planet(list)
+  
 end
 
 def long_planeteer_calls# code an argument here
