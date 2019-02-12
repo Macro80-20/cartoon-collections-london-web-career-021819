b5826fdc5c6e4dd6d83f9593a6f84f0e5fa255f2
fernando <fernando_lopezc@live.com>
2019-02-12 13:27:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ list.collect do
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+calls_longer_than_four_words = array.any? { |e| e.length> 4  }
 end
 
 def find_the_cheese# code an argument here
